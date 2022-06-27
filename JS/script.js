d43e875bc218ef8845fb71b728e7f2a6fb3240cd
AlaaEldinTarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2022-06-27 17:47:33 +0200

    up

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,30 @@
 // ########### Typing Animation ###########
 var typed = new Typed("#typing", {
-  strings:["","(Front-end) Web Developer",""],
-  typeSpeed:60,
-  backSpeed:60,
-  loop:true
-});
+  strings: ["", "(Front-end) Web Developer", ""],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop: true
+})
+// ########### Aside ###########
+let nav = document.querySelector(".nav"),
+  navList = nav.querySelectorAll("li"),
+  totalNavList = navList.length,
+  allSection = document.querySelectorAll(".section"),
+  totalSection = allSection.length;
+for (let i = 0; i < totalNavList; i++) {
+  let a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    for (let j = 0; j < totalNavList; j++) {
+      navList[j].querySelector("a").classList.remove("active");
+    }
+    this.classList.add("active")
+    showSection(this);
+  })
+}
+function showSection(e) {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("active");
+  }
+  let target = e.getAttribute("href").split("#")[1];
+  document.querySelector("#" + target).classList.add("active")
+}
